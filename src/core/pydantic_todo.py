220e06e2408ddb5cda407d60172bed0d212e1951
naughtmarrow <naughtmarrow@gmail.com>
2025-09-24 03:21:29 -0600

    flipped todo and user core models

--- src/core/pydantic_todo.py
+++ src/core/pydantic_todo.py
@@ -1,6 +1,14 @@
+from datetime import datetime
+from typing import Optional
 from pydantic import BaseModel
+
+from src.common import PriorityType
 
 class Todo(BaseModel):
     id: int
-    name: str
-    password: str
+    user_id: int
+    description: str
+    date_created: datetime
+    date_due: Optional[datetime]
+    priority: PriorityType
+    completed: bool
--- src/core/pydantic_user.py
+++ src/core/pydantic_user.py
@@ -1,16 +1,6 @@
-from datetime import datetime
-from typing import Optional
-
 from pydantic import BaseModel
-
-from src.common import PriorityType
-
 
 class User(BaseModel):
     id: int
-    user_id: int
-    description: str
-    date_created: datetime
-    date_due: Optional[datetime]
-    priority: PriorityType
-    completed: bool
+    name: str
+    password: str
